7e35088654171178d2b737c0b54e4bc3d065342c
kaliassistant <1325077@gmail.com>
2026-01-05 20:04:39 +0800

    min

--- scripts/addons/jack-coreutils/addon.mk
+++ scripts/addons/jack-coreutils/addon.mk
@@ -1,7 +1,7 @@
 $(BUILDDIR)/jack-coreutils-stamp:
 	@echo "$(COLOUR_GREEN)Installing jack-coreutils for $(BOARD)$(END_COLOUR)"
 	@mkdir -p /rootfs/tmp/install
-	@wget -O /output/lichee-jack-utils.tar.xz https://github.com/KaliAssistant/Lichee-Jack-utils/releases/download/2025.12-cb622c7/lichee-jack-utils_2025.12-cb622c7_all.tar.xz
+	@wget -O /output/lichee-jack-utils.tar.xz https://github.com/KaliAssistant/Lichee-Jack-utils/releases/download/2026.01-2c5e272/lichee-jack-utils_2026.01-2c5e272_all.tar.xz
 	@tar Jxvf /output/lichee-jack-utils.tar.xz -C /output
 	@cp /output/lichee-jack-utils/*.deb /rootfs/tmp/install
 	@touch $@
