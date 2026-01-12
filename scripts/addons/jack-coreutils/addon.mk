50f7b2bc627eee13e3ea990cc2a56ff7f553c4fa
kaliassistant <1325077@gmail.com>
2026-01-12 09:09:50 +0800

    update coreutils to f889c27

--- scripts/addons/jack-coreutils/addon.mk
+++ scripts/addons/jack-coreutils/addon.mk
@@ -1,7 +1,7 @@
 $(BUILDDIR)/jack-coreutils-stamp:
 	@echo "$(COLOUR_GREEN)Installing jack-coreutils for $(BOARD)$(END_COLOUR)"
 	@mkdir -p /rootfs/tmp/install
-	@wget -O /output/lichee-jack-utils.tar.xz https://github.com/KaliAssistant/Lichee-Jack-utils/releases/download/2026.01-0c12f4f/lichee-jack-utils_2026.01-0c12f4f_all.tar.xz
+	@wget -O /output/lichee-jack-utils.tar.xz https://github.com/KaliAssistant/Lichee-Jack-utils/releases/download/2026.01-f889c27/lichee-jack-utils_2026.01-f889c27_all.tar.xz
 	@tar Jxvf /output/lichee-jack-utils.tar.xz -C /output
 	@cp /output/lichee-jack-utils/*.deb /rootfs/tmp/install
 	@touch $@
