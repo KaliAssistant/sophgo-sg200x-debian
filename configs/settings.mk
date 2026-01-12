b3c0011162e1061a2287394dcb4eadccadf6260b
kaliassistant <1325077@gmail.com>
2026-01-12 09:42:42 +0800

    update kernelrev

--- configs/settings.mk
+++ configs/settings.mk
@@ -1,4 +1,4 @@
-KERNELREV="licheejack-test3"
+KERNELREV="licheejack-dev2"
 FSBLVERSION=1.1.0
 PACKAGES="ca-certificates debian-archive-keyring dosfstools binutils file tree sudo bash-completion u-boot-menu openssh-server network-manager dnsmasq-base libpam-systemd ppp libengine-pkcs11-openssl iptables systemd-timesyncd vim usbutils parted exfatprogs systemd-sysv i2c-tools net-tools ethtool avahi-utils sudo gnupg rsync gpiod u-boot-tools libubootenv-tool"
 
